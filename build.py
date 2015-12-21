4c0e70e944701bfd78df8bd1149cd1e96d30ece0
Daniel Baird <JamesDanielBaird@gmail.com>
2015-12-20 22:51:04 -0700

    push for debug

--- build.py
+++ build.py
@@ -17,6 +17,16 @@ with open("info.json") as dat:
     release_dir = os.path.join(release, full_name)
     os.makedirs(release_dir) # Prep
     copy_tree(res_dir, release_dir)
+    # get info json in there as well
+    # Create info.json
+    json_info_path = os.path.join(release_dir, 'info.json')
+    with open(json_info_path, 'w') as info_file:
+        info_string = json.dumps(info, indent=4, sort_keys=True)
+        info_file.write(info_string)
 
     # Generate zip release
-    shutil.make_archive(os.path.join(release, full_name), "zip", release, full_name)
+    print os.path.join(release, full_name)
+    print "zip"
+    print release
+    print release_dir
+    shutil.make_archive(os.path.join(release, full_name), "zip", release, release_dir)
